df28d6e14775392a6061d87593badc90163a5403
Totoro700 <Qwerty82994@gmail.com>
2023-01-19 13:48:43 -0600

    Add ask function
    need to expand answer possibilities

--- quiz.py
+++ quiz.py
@@ -1,3 +1,8 @@
+'''
+-----TODO-----
+Make ask function better to accept different answers
+
+'''
 import random, time, os, sys
 def t(t):
     for l in t:
@@ -5,3 +10,10 @@ def t(t):
         sys.stdout.flush()
         time.sleep(0.1)
     print('')
+
+def ask(q,a): #q: question, a: correct answer
+    if input(q) == a:
+        t("Congrats! You got it correct")
+    else:
+        t("You suck! You got it wrong")
+
